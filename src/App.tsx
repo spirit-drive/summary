9001a1fffa439380410cc54819a16b6e84ede235
Igor Zvyagin <spirit-drive@yandex.ru>
2021-07-24 14:59:15 +0300

    little improvements

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import { Head } from './Head';
 import { Navigation } from './navigation/Navigation';
-import i18n from './i18n';
+import { i18n } from './i18n';
 
 export const App: React.FC = () => (
   <BrowserRouter>
